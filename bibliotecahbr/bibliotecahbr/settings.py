d44c9269265369c90f80c44f5dca6c0c5f17c740
Kauã <119459549+kauandragues@users.noreply.github.com>
2025-11-12 12:38:43 -0300

    build: adicionado 'rest_framework' nos apps

--- bibliotecahbr/bibliotecahbr/settings.py
+++ bibliotecahbr/bibliotecahbr/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'biblioteca'
+    'biblioteca',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
