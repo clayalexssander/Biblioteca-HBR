61c4c4b8f4802c1b9aaef10bde9f5bb651e92af0
kauandragues <kaua.rodrigues1509@gmail.com>
2025-11-19 15:41:31 -0300

    feat: criação da funcionalidade 'devolver emprestimo'

--- bibliotecahbr/biblioteca/urls.py
+++ bibliotecahbr/biblioteca/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .views import livro_view
+from .views import livro_view, usuario_view, emprestimo_view
 from django.urls import path, include
 
 urlpatterns = [
     path('listar_livros', livro_view.listar_livros),
+    path('devolver_emprestimo/<int:pk>', emprestimo_view.devolver_emprestimo)
 ]
--- bibliotecahbr/biblioteca/views/emprestimo_view.py
+++ bibliotecahbr/biblioteca/views/emprestimo_view.py
@@ -2,9 +2,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from ..models import Emprestimo
+from ..models import Emprestimo, Livro
 from ..serializers.emprestimo_serializer import EmprestimoSerializer
+from ..serializers.livro_serializer import LivroSerializer
 
+from django.utils import timezone 
+from pytz import timezone as tz
+from django.db import transaction
 """
 Cadastrar usuario - (POST)
 cadastrar livro - (POST)
@@ -30,3 +34,41 @@ def listar_livros(request):
         return Response(serializer.data)
         
     return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def devolver_emprestimo(request, pk):
+    if request.method == 'POST':
+        
+        fuso_horario_sp = tz('America/Sao_Paulo')
+        timezone.activate(fuso_horario_sp)
+
+        try:
+            emprestimo = Emprestimo.objects.get(pk=pk)
+        except Emprestimo.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        if emprestimo.status == "Finalizado":
+            timezone.deactivate()
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            with transaction.atomic():
+
+                emprestimo.data_dev = timezone.localtime(timezone.now()).date()
+                emprestimo.status = 'Finalizado'
+                emprestimo.save()
+                
+                livro = Livro.objects.get(pk=emprestimo.id_livro_id)
+                livro.disponivel = "Disponível"
+                livro.save()
+                
+        except Livro.DoesNotExist:
+            timezone.deactivate()
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        timezone.deactivate()
+        
+        return Response(status=status.HTTP_200_OK)
+    
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+        
