4cd579b78413f48f5e848d9bd5c818ed86b9abf5
BeatrizModesto <beatriz.modestot@gmail.com>
2025-12-12 20:35:27 -0300

    feat: adicionei requisição POST para cadastro de livros

--- bibliotecahbr/index.py
+++ bibliotecahbr/index.py
@@ -1,9 +1,34 @@
 import requests
 
-url ="http://127.0.0.1:8000/api/listar_livros?format=json"
+# cadastro de livros
+url_cadastroLivro = "http://127.0.0.1:8000/api/livros/cadastrar?format=json"
 
-response = requests.get(url)
+titulo = input("Título: ")
+autor = input("Autor: ")
+ano = input("Ano: ")
+isbn = input("ISBN: ")
+
+dados = {
+"titulo": titulo,
+"autor": autor,
+"ano": int(ano),
+"isbn": isbn
+}
+
+response = requests.post(url_cadastroLivro, json=dados)
+
+print("Status:", response.status_code)
+print("Resposta:", response.json())
+
+
+
+# listar todos os livros 
+url_listarLivro ="http://127.0.0.1:8000/api/listar_livros?format=json"
+
+response = requests.get(url_listarLivro)
 
 print(response.json())
 print("Resposta:",response.status_code)
+
+
 
